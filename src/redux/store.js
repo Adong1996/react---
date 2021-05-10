09ef30e153bc0ce54e7d5584f7a06378b56d946b
chendong <646392218@qq.com>
2021-05-10 09:48:43 +0800

    undate

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+//最核心的管理者
+//reudx 中引入createrStore ，用于创建最核心的store对象
+import {createStore, applyMiddleware} from 'redux'
+//引入reducer
+import reducers from './reducers'
+//引入 redux-thunk
+import thunk from 'react-thunk'
+//引入 redux-devtools-extension,  用于支持 redux 开发者调试工具的运行
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+export default createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
