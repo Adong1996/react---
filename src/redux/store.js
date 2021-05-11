85b0618b3a3d913901315ec7c3bb5e16fd0db542
chendong <646392218@qq.com>
2021-05-11 18:49:28 +0800

    undata

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import {createStore, applyMiddleware} from 'redux'
 //引入reducer
 import reducers from './reducers/index.js'
 //引入 redux-thunk
-import thunk from 'react-thunk'
+import thunk from 'redux-thunk'
 //引入 redux-devtools-extension,  用于支持 redux 开发者调试工具的运行
 import {composeWithDevTools} from 'redux-devtools-extension'
 
